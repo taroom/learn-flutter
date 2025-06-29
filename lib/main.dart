04356e3b442ccefae672011f3cd380d32774293f
Sultan Room <sultanroomcode@gmail.com>
2025-06-29 20:55:37 +0700

    [ROM] Implement ListView with JSON data fetching and update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  runApp(MaterialApp(
+  /* runApp(MaterialApp(
     title: 'ListView Array App',
     home: ListViewArrayApp(
       data: List<String>.generate(300, (i) => "Item $i"),
     ),
-  ));
+  )); */
+
+  runApp(MaterialApp(title: 'ListView JSON App', home: ListViewJsonApp()));
 }
 
+/* use json */
+class ListViewJsonApp extends StatefulWidget {
+  const ListViewJsonApp({super.key});
+
+  @override
+  State<ListViewJsonApp> createState() => _ListViewJsonAppState();
+}
+
+class _ListViewJsonAppState extends State<ListViewJsonApp> {
+  List _dataJSON = [];
+  bool _isLoading = true;
+
+  Future<String> getData() async {
+    http.Response response = await http.get(
+        Uri.parse("https://jsonplaceholder.typicode.com/posts"),
+        headers: {"Accept": "application/json"});
+
+    if (response.statusCode == 200) {
+      setState(() {
+        _dataJSON = json.decode(response.body);
+        _isLoading = false;
+      });
+      return "Success";
+    } else {
+      throw Exception('Gagal memuat data');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("List View With JSON Data")),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: _dataJSON.length,
+              itemBuilder: (context, index) {
+                final data = _dataJSON[index];
+                return Card(child: Text(data['title']));
+              },
+            ),
+    );
+  }
+}
+
+/*
+ * use array
+ */
 class ListViewArrayApp extends StatelessWidget {
   const ListViewArrayApp({super.key, required this.data});
 
