cb73980f8ffdb0edbc453d49073097c0157a6e98
Sultan Room <sultanroomcode@gmail.com>
2025-06-29 18:30:55 +0700

    [ROM] add simple list view example

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,51 @@ import 'package:flutter/material.dart';
 import 'tabs/tab_example.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ListViewApp());
+  // runApp(const TabBarApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ListViewApp extends StatelessWidget {
+  const ListViewApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('ListView Stateful'),
+        backgroundColor: Colors.amber,
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.speaker),
+            title: Text("Speaker"),
+          ),
+          ListTile(
+            leading: Icon(Icons.radio),
+            title: Text("Radio"),
+          ),
+          ListTile(
+            leading: Icon(Icons.smartphone),
+            title: Text("Smart Phone"),
+          ),
+          ListTile(
+            leading: Icon(Icons.alarm),
+            title: Text("Alarm"),
+          ),
+          ListTile(
+            leading: Icon(Icons.air),
+            title: Text("Air"),
+          ),
+        ],
+      ),
+    ));
+  }
+}
+
+class TabBarApp extends StatelessWidget {
+  const TabBarApp({super.key});
 
   @override
   Widget build(BuildContext context) {
