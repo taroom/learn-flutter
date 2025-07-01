51435e3581d33de04da2f0f060cb8d2b8a0efd94
Sultan Room <sultanroomcode@gmail.com>
2025-07-01 23:24:02 +0700

    [ROM] Refactor SnackBarApp to include input fields for text, alert, and snackbar functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,99 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 void main() {
-  /* runApp(MaterialApp(
-    title: 'ListView Array App',
-    home: ListViewArrayApp(
-      data: List<String>.generate(300, (i) => "Item $i"),
-    ),
-  )); */
-
-  runApp(MaterialApp(title: 'ListView JSON App', home: ListViewJsonApp()));
+  runApp(MaterialApp(title: 'ListView JSON App', home: SnackBarApp()));
 }
 
-/* use json */
-class ListViewJsonApp extends StatefulWidget {
-  const ListViewJsonApp({super.key});
+class SnackBarApp extends StatefulWidget {
+  const SnackBarApp({super.key});
 
   @override
-  State<ListViewJsonApp> createState() => _ListViewJsonAppState();
+  State<SnackBarApp> createState() => _SnackBarAppState();
 }
 
-class _ListViewJsonAppState extends State<ListViewJsonApp> {
-  List _dataJSON = [];
-  bool _isLoading = true;
-
-  Future<String> getData() async {
-    http.Response response = await http.get(
-        Uri.parse("https://jsonplaceholder.typicode.com/posts"),
-        headers: {"Accept": "application/json"});
-
-    if (response.statusCode == 200) {
-      setState(() {
-        _dataJSON = json.decode(response.body);
-        _isLoading = false;
-      });
-      return "Success";
-    } else {
-      throw Exception('Gagal memuat data');
+class _SnackBarAppState extends State<SnackBarApp> {
+  // set variable for save text
+  String inputText = '';
+  TextEditingController tEditingcontroller = TextEditingController();
+  TextEditingController tAlertEditingcontroller = TextEditingController();
+  TextEditingController tSnackBarEditingcontroller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    alertOnPress(String str) {
+      if (str.isEmpty) return;
+      showDialog(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+                title: Text('Alert'),
+                content: Text('Anda mengetikkan $str'),
+                actions: <Widget>[
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('OK'))
+                ],
+              ));
     }
-  }
 
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
+    void snackBarFunc(String str) {
+      if (str.isEmpty) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Anda mengetikkan $str'),
+          duration: Duration(seconds: 3),
+        ),
+      );
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("List View With JSON Data")),
-      body: _isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: _dataJSON.length,
-              itemBuilder: (context, index) {
-                final data = _dataJSON[index];
-                return Container(
-                    padding: EdgeInsets.all(10),
-                    child: Card(
-                        child: Container(
-                      padding: EdgeInsets.all(10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            data['title'],
-                            style: TextStyle(color: Colors.blue, fontSize: 20),
-                          ),
-                          Text(
-                            data['body'],
-                            style: TextStyle(color: Colors.grey, fontSize: 10),
-                          ),
-                        ],
-                      ),
-                    )));
-              },
-            ),
-    );
-  }
-}
-
-/*
- * use array
- */
-class ListViewArrayApp extends StatelessWidget {
-  const ListViewArrayApp({super.key, required this.data});
-
-  final List<String> data;
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("List View Array"),
-        backgroundColor: Colors.amber,
+        title: Text(
+          'Input Text, Alert Widget and Snackbar',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.purple,
       ),
-      body: ListView.builder(
-          itemCount: data.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: Icon(Icons.widgets),
-              title: Text(data[index]),
-            );
-          }),
+      body: Container(
+        padding: EdgeInsets.all(5),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: tEditingcontroller,
+              decoration:
+                  InputDecoration(hintText: "Bisa tuliskan nama Anda disini?"),
+              onSubmitted: (String txt) {
+                setState(() {
+                  inputText = '$txt\n$inputText';
+                  tEditingcontroller.text = "";
+                });
+              },
+            ),
+            Text(
+              inputText,
+              style: TextStyle(fontSize: 20),
+            ),
+            TextField(
+              controller: tAlertEditingcontroller,
+              decoration: InputDecoration(
+                  hintText: "Bisa tuliskan nama Anda disini untuk Alert?"),
+              onSubmitted: (String txt) {
+                tAlertEditingcontroller.text = '';
+                alertOnPress(txt);
+              },
+            ),
+            TextField(
+              controller: tSnackBarEditingcontroller,
+              decoration: InputDecoration(
+                  hintText: "Bisa tuliskan nama Anda disini untuk Snack Bar?"),
+              onSubmitted: (String txt) {
+                tSnackBarEditingcontroller.text = '';
+                snackBarFunc(txt);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
